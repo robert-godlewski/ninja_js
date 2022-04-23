7d9e4f5784dbaf6e1339632cdb387ee3a315cdd1
Robert Godlewski <robtechg5@gmail.com>
2022-04-23 16:16:24 -0700

    flushed out ninja class.

--- ninja.js
+++ ninja.js
@@ -1,1 +1,29 @@
-class Ninja {}
+class Ninja {
+    constructor(name, health) {
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName() {
+        console.log(`${this.name}`);
+    }
+
+    showStats() {
+        console.log(`Stats for ${this.name}:`);
+        console.log(`health = ${this.health}`);
+        console.log(`speed = ${this.speed}`);
+        console.log(`strength = ${this.strength}`);
+    }
+
+    drinkSake() {
+        this.health += 10;
+        console.log(`health is now ${this.health}`);
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa", 100);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
